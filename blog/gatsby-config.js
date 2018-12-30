38ce86545c4e5509f3eabf86b018ad7873888c06
Konstantin <mail@konstantin.io>
2018-12-30 23:21:33 +0100

    gatsby blog initial files

---
+++ blog/gatsby-config.js
@@ -0,0 +1,85 @@
+module.exports = {
+  siteMetadata: {
+    title: `Konstantin Gorodinskiy`,
+    author: `Konstantin Gorodinskiy`,
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://konstantin.io/`,
+    keywords: ['gorodinskiy', 'konstantin', 'gko', 'developer', 'web'],
+    social: {
+      twitter: 'konstantin',
+      github: 'gko',
+      codepen: 'konstantin',
+      linkedin: 'gorodinskiy',
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-55314257-2`,
+      },
+    },
+    // `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Konstantin Gorodinskiy`,
+        short_name: `Konstantin`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#333333`,
+        display: `minimal-ui`,
+        icon: `content/assets/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+  ],
+}

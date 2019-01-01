d006ddc8271abc7ae03352f92e90c08acd34a967
Konstantin Gorodinskiy <mail@konstantin.io>
2019-01-01 20:17:24 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-df3ab70941eee37829c7.js"
+    "url": "webpack-runtime-07b9f5100f19009185b5.js"
   },
   {
     "url": "app.c625e6a95d20abe0a981.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "23272e6acde2ed9178d32faf3aef3a53"
+    "revision": "2bdfc43d3687c665e6b3006040d14ec6"
   },
   {
     "url": "component---src-pages-404-js-5c62197e0a764e4a98eb.js"

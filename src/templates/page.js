5d3527df004a4f81e03014984241bdc2ef564347
Konstantin <mail@konstantin.io>
2019-01-01 14:00:41 +0100

    [feat] page template

---
+++ src/templates/page.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+
+import Bio from '../components/Bio'
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+
+class PageTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title={post.frontmatter.title} description={post.excerpt} />
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Layout>
+    )
+  }
+}
+
+export default PageTemplate
+
+export const pageQuery = graphql`
+  query PageBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`

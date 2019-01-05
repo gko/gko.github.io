320a2a458c99ac6c8af395d3cfa153a1fb663949
Konstantin Gorodinskiy <mail@konstantin.io>
2019-01-05 19:35:59 +0100

    [tech] seo → SEO

--- src/templates/page.js
+++ src/templates/page.js
@@ -3,7 +3,7 @@ import { Link, graphql } from 'gatsby'
 
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
-import SEO from '../components/seo'
+import SEO from '../components/SEO'
 
 class PageTemplate extends React.Component {
   render() {

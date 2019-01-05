c7e05f1a5e738bccb84f4d6c2d9507ddecb2a128
Konstantin Gorodinskiy <mail@konstantin.io>
2019-01-05 23:37:04 +0100

    [feat] get year when publishing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => (<footer>
-	© 2018,&nbsp;<a href="mailto:hi@konstantin.io?subject=hello">Konstantin</a>
+	© {(new Date()).getFullYear()},&nbsp;<a href="mailto:hi@konstantin.io?subject=hello">Konstantin</a>
 </footer>)
 
 export default Footer;

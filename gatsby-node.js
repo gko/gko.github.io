9ef3483f75a5c0145e779807aa1160e8e0d2a54d
Konstantin Gorodinskiy <mail@konstantin.io>
2019-01-07 00:19:33 +0100

    [feat] createPreview with node-canvas

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,115 +1,194 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const _ = require('lodash')
+const { registerFont, createCanvas } = require('canvas')
+const fs = require('fs')
+
+function getLines(ctx, phrase, maxPxLength, textStyle) {
+    var wa = phrase.split(' '),
+        phraseArray = [],
+        lastPhrase = wa[0],
+        measure = 0,
+        splitChar = ' '
+
+    if (wa.length <= 1) {
+        return wa
+    }
+
+    ctx.font = textStyle
+    for (var i = 1; i < wa.length; i++) {
+        var w = wa[i]
+        measure = ctx.measureText(lastPhrase + splitChar + w).width
+        if (measure < maxPxLength) {
+            lastPhrase += splitChar + w
+        } else {
+            phraseArray.push(lastPhrase)
+            lastPhrase = w
+        }
+        if (i === wa.length - 1) {
+            phraseArray.push(lastPhrase)
+            break
+        }
+    }
+    return phraseArray
+}
+
+registerFont(path.join(__dirname, 'static/fonts/pt.ttf'), {
+    family: 'PT Serif',
+})
 
 const isBlogPost = ({ node: { fileAbsolutePath } }) =>
-  RegExp('(/src/posts)/.*\\.md$').test(fileAbsolutePath)
+    RegExp('(/src/posts)/.*\\.md$').test(fileAbsolutePath)
 
-const isPublished = ({ node: { frontmatter: { published = false } } }) => published
+const isPublished = ({
+    node: {
+        frontmatter: { published = false },
+    },
+}) => published
+
+const createPreview = (title, fileName) => {
+    return new Promise((res, rej) => {
+		const width = 500 
+        const canvas = createCanvas(width, width / 2)
+        const ctx = canvas.getContext('2d')
+
+        ctx.fillStyle = 'white'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+		const fontSize = 30
+        const fontStyle = `${fontSize}px PT Serif`
+		ctx.font = fontStyle
+		ctx.textAlign = "center";
+        ctx.fillStyle = 'black'
+
+        const text = getLines(ctx, title, width, fontStyle)
+
+		text.forEach((line, i) => 
+        	ctx.fillText(line, canvas.width / 2, canvas.height / 2 + fontSize * i)
+		);
+
+        const out = fs.createWriteStream(
+            path.join(__dirname, 'public/', _.kebabCase(title) + '.png')
+        )
+
+        const stream = canvas.createPNGStream()
+        stream.pipe(out)
+        out.on('finish', () => res())
+    })
+}
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+    const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve(`./src/templates/blog-post.js`)
-    const page = path.resolve(`./src/templates/page.js`)
-    const tagTemplate = path.resolve('src/templates/tags.js')
+    return new Promise((resolve, reject) => {
+        const blogPost = path.resolve(`./src/templates/blog-post.js`)
+        const page = path.resolve(`./src/templates/page.js`)
+        const tagTemplate = path.resolve('src/templates/tags.js')
 
-    resolve(
-      graphql(
-        `
-          {
-            allMarkdownRemark(
-							sort: { order: DESC, fields: [frontmatter___date] }
-							limit: 2000
-            ) {
-              edges {
-                node {
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    tags
-										published
-                  }
-                  fileAbsolutePath
+        resolve(
+            graphql(
+                `
+                    {
+                        allMarkdownRemark(
+                            sort: { order: DESC, fields: [frontmatter___date] }
+                            limit: 2000
+                        ) {
+                            edges {
+                                node {
+                                    fields {
+                                        slug
+                                    }
+                                    frontmatter {
+                                        title
+                                        tags
+                                        published
+                                    }
+                                    fileAbsolutePath
+                                }
+                            }
+                        }
+                    }
+                `
+            ).then(result => {
+                if (result.errors) {
+                    console.log(result.errors)
+                    reject(result.errors)
                 }
-              }
-            }
-          }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
 
-        const allPosts = result.data.allMarkdownRemark.edges
-        const posts = allPosts.filter(isBlogPost).filter(isPublished)
-        const pages = allPosts.filter(p => !isBlogPost(p))
+                const allPosts = result.data.allMarkdownRemark.edges
+                const posts = allPosts.filter(isBlogPost).filter(isPublished)
+                const pages = allPosts.filter(p => !isBlogPost(p))
 
-        posts.forEach((post, index) => {
-          const previous =
-            index === posts.length - 1 ? null : posts[index + 1].node
-          const next = index === 0 ? null : posts[index - 1].node
+                posts.forEach(async (post, index) => {
+                    const previous =
+                        index === posts.length - 1
+                            ? null
+                            : posts[index + 1].node
+                    const next = index === 0 ? null : posts[index - 1].node
 
-          createPage({
-            path: post.node.fields.slug,
-            component: blogPost,
-            context: {
-              slug: post.node.fields.slug,
-              previous,
-              next,
-            },
-          })
-        })
+                    createPage({
+                        path: post.node.fields.slug,
+                        component: blogPost,
+                        context: {
+                            slug: post.node.fields.slug,
+							twitterPreview: 
+                            previous,
+                            next,
+                        },
+                    })
 
-        pages.forEach(post => {
-          createPage({
-            path: post.node.fields.slug,
-            component: page,
-            context: {
-              slug: post.node.fields.slug,
-            },
-          })
-        })
+                    await createPreview(
+                        post.node.frontmatter.title,
+                        post.node.fields.slug
+                    )
+                })
 
-        let tags = []
+                pages.forEach(post => {
+                    createPage({
+                        path: post.node.fields.slug,
+                        component: page,
+                        context: {
+                            slug: post.node.fields.slug,
+                        },
+                    })
+                })
 
-        _.each(posts, post => {
-          if (_.get(post, 'node.frontmatter.tags')) {
-            tags = tags.concat(post.node.frontmatter.tags)
-          }
-        })
+                let tags = []
 
-        tags = _.uniq(tags)
+                _.each(posts, post => {
+                    if (_.get(post, 'node.frontmatter.tags')) {
+                        tags = tags.concat(post.node.frontmatter.tags)
+                    }
+                })
 
-        tags.forEach(tag => {
-          createPage({
-            path: `/tags/${_.kebabCase(tag)}/`,
-            component: tagTemplate,
-            context: {
-              tag,
-            },
-          })
-        })
-      })
-    )
-  })
+                tags = _.uniq(tags)
+
+                tags.forEach(tag => {
+                    createPage({
+                        path: `/tags/${_.kebabCase(tag)}/`,
+                        component: tagTemplate,
+                        context: {
+                            tag,
+                        },
+                    })
+                })
+            })
+        )
+    })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+    const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({
-      node,
-      getNode,
-    })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
+    if (node.internal.type === `MarkdownRemark`) {
+        const value = createFilePath({
+            node,
+            getNode,
+        })
+        createNodeField({
+            name: `slug`,
+            node,
+            value,
+        })
+    }
 }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,106 +4,119 @@ import { Link, graphql } from 'gatsby'
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
+import { Helmet } from 'react-helmet'
 
-import kebabCase from "lodash/kebabCase"
+import kebabCase from 'lodash/kebabCase'
 
-	//</Link>
+//</Link>
 class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const { siteUrl, title: siteTitle } = this.props.data.site.siteMetadata
-    const { previous, next, slug } = this.props.pageContext
-    const tweet = encodeURIComponent(`${siteUrl}${slug}`)
-    const { title, tags, date } = post.frontmatter
+    render() {
+        const post = this.props.data.markdownRemark
+        const { siteUrl, title: siteTitle } = this.props.data.site.siteMetadata
+        const { previous, next, slug } = this.props.pageContext
+        const tweet = encodeURIComponent(`${siteUrl}${slug}`)
+        const { title, tags, date } = post.frontmatter
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title={title} description={post.excerpt} />
-        <h1>{post.frontmatter.title}</h1>
-        {tags && (
-          <ul
-            className="tags"
-            style={{
-              paddingLeft: 0,
-            }}
-          >
-            {tags.map(tag => (
-							<li key={tag} className="tag">
-								<Link to={`/tags/${kebabCase(tag)}/`}>
-									{tag}
-								</Link>
-              </li>
-						))}
-          </ul>
-        )}
-        <p>{date}</p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr />
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <SEO title={title} description={post.excerpt} />
+                <Helmet title={title}>
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta
+                        name="twitter:image"
+                        content={`${siteUrl}/${kebabCase(title)}.png`}
+                    />
+                </Helmet>
 
-        <p className="share">
-          <a
-            className="twitter"
-            href={`https://twitter.com/intent/tweet?text=${tweet}`}
-          >
-            Tweet{' '}
-            <img align="absmiddle" src="/twitter-logo.svg" alt="twitter logo" />
-          </a>
-        </p>
+                <h1>{post.frontmatter.title}</h1>
+                {tags && (
+                    <ul
+                        className="tags"
+                        style={{
+                            paddingLeft: 0,
+                        }}
+                    >
+                        {tags.map(tag => (
+                            <li key={tag} className="tag">
+                                <Link to={`/tags/${kebabCase(tag)}/`}>
+                                    {tag}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+                <p>{date}</p>
+                <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                <hr />
 
-        <ul
-          className="navigation"
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-            margin: '2rem 0 0',
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                <span className="arrow">←</span> {title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {title} <span className="arrow">→</span>
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    )
-  }
+                <p className="share">
+                    <a
+                        className="twitter"
+                        href={`https://twitter.com/intent/tweet?text=${tweet}`}
+                    >
+                        Tweet{' '}
+                        <img
+                            align="absmiddle"
+                            src="/twitter-logo.svg"
+                            alt="twitter logo"
+                        />
+                    </a>
+                </p>
+
+                <ul
+                    className="navigation"
+                    style={{
+                        display: `flex`,
+                        flexWrap: `wrap`,
+                        justifyContent: `space-between`,
+                        listStyle: `none`,
+                        padding: 0,
+                        margin: '2rem 0 0',
+                    }}
+                >
+                    <li>
+                        {previous && (
+                            <Link to={previous.fields.slug} rel="prev">
+                                <span className="arrow">←</span> {title}
+                            </Link>
+                        )}
+                    </li>
+                    <li>
+                        {next && (
+                            <Link to={next.fields.slug} rel="next">
+                                {title} <span className="arrow">→</span>
+                            </Link>
+                        )}
+                    </li>
+                </ul>
+            </Layout>
+        )
+    }
 }
 
 export default BlogPostTemplate
 
 export const pageQuery = graphql`
-  query BlogPostBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-        author
-        siteUrl
-      }
+    query BlogPostBySlug($slug: String!) {
+        site {
+            siteMetadata {
+                title
+                author
+                siteUrl
+            }
+        }
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            id
+            excerpt(pruneLength: 160)
+            html
+            frontmatter {
+                title
+                tags
+                date(formatString: "MMMM DD, YYYY")
+            }
+            fields {
+                slug
+            }
+        }
     }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        tags
-        date(formatString: "MMMM DD, YYYY")
-      }
-      fields {
-        slug
-      }
-    }
-  }
 `

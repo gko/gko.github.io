d0a6a5826062fd0d7efe98074da426a65c10e7c9
Konstantin Gorodinskiy <mail@konstantin.io>
2019-01-07 00:19:14 +0100

    [tech] refactor pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,32 +6,40 @@ import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
 class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props
-    const { title, keywords } = data.site.siteMetadata
-    const posts = data.posts.edges
+    render() {
+        const { data } = this.props
+        const { title, keywords } = data.site.siteMetadata
+        const posts = data.posts.edges
 
-    return (
-      <Layout location={this.props.location} title={title}>
-        <SEO title="Konstantin" keywords={keywords} />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3>
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-							<small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          )
-        })}
-      </Layout>
-    )
-  }
+        return (
+            <Layout location={this.props.location} title={title}>
+                <SEO title="Konstantin" keywords={keywords} />
+
+                <Bio />
+                {posts.map(({ node }) => {
+                    const title = node.frontmatter.title || node.fields.slug
+                    return (
+                        <div key={node.fields.slug}>
+                            <h3>
+                                <Link
+                                    style={{ boxShadow: `none` }}
+                                    to={node.fields.slug}
+                                >
+                                    {title}
+                                </Link>
+                            </h3>
+                            <small>{node.frontmatter.date}</small>
+                            <p
+                                dangerouslySetInnerHTML={{
+                                    __html: node.excerpt,
+                                }}
+                            />
+                        </div>
+                    )
+                })}
+            </Layout>
+        )
+    }
 }
 
 export default BlogIndex
@@ -40,7 +48,7 @@ export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
-        title,
+        title
         keywords
       }
     }
@@ -51,35 +59,35 @@ export const pageQuery = graphql`
 				frontmatter: {
 					published: {
 						ne: false
-						ne: null			
+						ne: null
 					}
 				}
 			}
-    ) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
+        ) {
+          edges {
+            node {
+              excerpt
+              fields {
+                slug
+              }
+              frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                title
+              }
+            }
           }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
+        }
+        pages: allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC },
+          filter: { fileAbsolutePath: { regex: "/(\/src\/pages)/.*\\.md$/" }}
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
           }
         }
       }
-    }
-    pages: allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC },
-      filter: { fileAbsolutePath: { regex: "/(\/src\/pages)/.*\\.md$/" }}
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  }
 `

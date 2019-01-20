3a1f35793c07799bbc22d13c97c04a22d05f497a
Konstantin <mail@konstantin.io>
2019-01-20 22:38:32 +0100

    [tech] refactor

--- src/html.js
+++ src/html.js
@@ -2,34 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class HTML extends React.Component {
-  render() {
-    return (
-      <html {...this.props.htmlAttributes}>
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <meta name="google-site-verification" content="UnUpmjHYK-ovLPrET8_xJ_XgJnmCyQcrYxkWkN06Z0k" />
-          {this.props.headComponents}{' '}
-        </head>{' '}
-        <body {...this.props.bodyAttributes}>
-          {' '}
-          {this.props.preBodyComponents}{' '}
-          <div
-            key={`body`}
-            id="___gatsby"
-            dangerouslySetInnerHTML={{
-              __html: this.props.body,
-            }}
-          />{' '}
-          {this.props.postBodyComponents}{' '}
-        </body>{' '}
-      </html>
-    )
-  }
+    render() {
+        return (
+            <html {...this.props.htmlAttributes}>
+                <head>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    <meta
+                        name="google-site-verification"
+                        content="UnUpmjHYK-ovLPrET8_xJ_XgJnmCyQcrYxkWkN06Z0k"
+                    />
+                    {this.props.headComponents}{' '}
+                </head>{' '}
+                <body {...this.props.bodyAttributes}>
+                    {' '}
+                    {this.props.preBodyComponents}{' '}
+                    <div
+                        key={`body`}
+                        id="___gatsby"
+                        dangerouslySetInnerHTML={{
+                            __html: this.props.body,
+                        }}
+                    />{' '}
+                    {this.props.postBodyComponents}{' '}
+                </body>{' '}
+            </html>
+        )
+    }
 }
 
-export default HTML;
+export default HTML

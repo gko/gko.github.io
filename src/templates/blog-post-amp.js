2456a4a638a25e81b53ef91be9cf74c51a6676df
Konstantin <mail@konstantin.io>
2019-01-27 10:42:35 +0100

    [tech] refactor

--- src/templates/blog-post-amp.js
+++ src/templates/blog-post-amp.js
@@ -29,8 +29,8 @@ class BlogPostTemplate extends React.Component {
                     type="article"
                 />
                 <Helmet title={title}>
-                    <html amp="true" />
-                    <script type='text/javascript' src='//cdn.ampproject.org/v0.js' async></script>
+                    <html amp />
+                    <script async src="https://cdn.ampproject.org/v0.js"></script>
                     <style amp-boilerplate="">{`body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}`}</style>
                     <noscript>{`<style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style>`}</noscript>
                     <meta name="twitter:card" content="summary_large_image" />
